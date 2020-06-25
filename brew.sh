ec703c781c96cf24b73c8f9f5f5d5d50f7ac76f3
Satie Sharma <239727+satie@users.noreply.github.com>
2020-06-24 20:50:34 -0700

    Updates for dev tools

--- brew.sh
+++ brew.sh
@@ -39,44 +39,44 @@ brew install wget --with-iri
 brew install gnupg
 
 # Install more recent versions of some macOS tools.
-brew install vim --with-override-system-vi
+# brew install vim --with-override-system-vi
 brew install grep
 brew install openssh
 brew install screen
-brew install php
+# brew install php
 brew install gmp
 
 # Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
+# brew tap bramstein/webfonttools
+# brew install sfnt2woff
+# brew install sfnt2woff-zopfli
+# brew install woff2
 
 # Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
+# brew install aircrack-ng
+# brew install bfg
+# brew install binutils
+# brew install binwalk
+# brew install cifer
+# brew install dex2jar
+# brew install dns2tcp
+# brew install fcrackzip
+# brew install foremost
+# brew install hashpump
+# brew install hydra
+# brew install john
+# brew install knock
+# brew install netpbm
 brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
+# brew install pngcheck
+# brew install socat
+# brew install sqlmap
+# brew install tcpflow
+# brew install tcpreplay
+# brew install tcptrace
+# brew install ucspi-tcp # `tcpserver` etc.
+# brew install xpdf
+# brew install xz
 
 # Install other useful binaries.
 brew install ack
@@ -85,17 +85,39 @@ brew install git
 brew install git-lfs
 brew install gs
 brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
+# brew install lua
+# brew install lynx
+# brew install p7zip
+# brew install pigz
+# brew install pv
 brew install rename
-brew install rlwrap
+# brew install rlwrap
 brew install ssh-copy-id
 brew install tree
-brew install vbindiff
+# brew install vbindiff
 brew install zopfli
+
+# Install dev tools
+brew install yarn
+brew install make
+brew install nvm
+
+brew cask install \
+  visual-studio-code \
+  google-chrome \
+  firefox \
+  docker \
+  vlc \
+  slack \
+  spotify \
+  postman
+	
+brew install hyperkit
+brew install minikube
+brew install awscli
+
+# zsh
+brew install zsh
 
 # Remove outdated versions from the cellar.
 brew cleanup
