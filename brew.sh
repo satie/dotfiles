3b8dcdc8d7651c7e508b0665ba926c46de868034
Satie Sharma <239727+satie@users.noreply.github.com>
2020-06-24 21:17:58 -0700

    Install Homebrew

--- brew.sh
+++ brew.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Install Homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # Install command-line tools using Homebrew.
 
